adecc0c8072f31457fbafc10ac182dfd4237bd75
ljc1928 <ljc1928@naver.com>
2021-04-22 23:11:33 +0900

    random library

---
+++ Nadocoding_random.py
@@ -0,0 +1,28 @@
+from random import * # 랜덤 라이브러리를 사용한다라는 뜻으로 지금은 사용법만
+
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+# print(int(random() * 10)) # 소수점 제외, 0 ~ 10 미만의 값 생성
+# print(int(random() * 10) + 1) # 시작 숫자가 0이 아닌 1, 1 ~ 10 이하의 값 생성
+
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+
+# 아래가 가장 효율이 좋아보임
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
