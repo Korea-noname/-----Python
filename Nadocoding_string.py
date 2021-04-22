3d35ab8039ee464858f17e35aeccd863b6a86fe6
ljc1928 <ljc1928@naver.com>
2021-04-22 23:38:39 +0900

    string

---
+++ Nadocoding_string.py
@@ -0,0 +1,11 @@
+sentence = '나는 남자다' # '' 문자열
+print(sentence)
+
+sentence2 = "파이썬은 쉬워요" # "" 문자열
+print(sentence2)
+
+sentence3 = """
+나는 남자고, 파이썬은 쉬워요
+열심히 할게요
+""" # 줄바꿈(위아래 공백 포함), 4줄이 출력
+print(sentence3)
